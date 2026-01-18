31c55a37c57d54e7612fc3fb37a11963f7b3f6f6
zuralon <106532526+zuralon@users.noreply.github.com>
2026-01-18 18:18:17 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,13 +1,15 @@
-export const config = {
-  runtime: "nodejs",
-};
+export const runtime = "nodejs";
 
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  const { password } = req.body;
+  const password = req.headers["x-admin-password"];
+
+  if (!password) {
+    return res.status(401).json({ success: false });
+  }
 
   if (password === process.env.ADMIN_PASSWORD) {
     return res.status(200).json({ success: true });
